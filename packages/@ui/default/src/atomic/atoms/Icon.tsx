d3f7c71ad356f34cb9b461005d64e08acbd33570
Jang Byeong Hyun <jang2162_@naver.com>
2024-03-19 19:02:11 +0900

    icon 작업중

--- packages/@ui/default/src/atomic/atoms/Icon.tsx
+++ packages/@ui/default/src/atomic/atoms/Icon.tsx
@@ -1,24 +1,28 @@
+import { HTMLAttributes } from 'react';
+
 import { cva } from 'class-variance-authority';
 import type { MaterialSymbol } from 'material-symbols';
 
+import { twMerge } from 'tailwind-merge';
+
 import { SIZE } from '../../constant';
 
-type IconProps = { type: string; size?: SIZE } & {
+type IconProps = HTMLAttributes<{}> & { type: string; size?: SIZE } & {
     type?: 'material-symbols-outlined' | 'material-symbols-rounded' | 'material-symbols-sharp';
     fill?: boolean;
-    weight: 100 | 200 | 300 | 400 | 500 | 600 | 700;
-    grade: -25 | 0 | 200;
-    opticalSize: 20 | 24 | 40 | 48;
+    weight?: 100 | 200 | 300 | 400 | 500 | 600 | 700;
+    grade?: -25 | 0 | 200;
+    opticalSize?: 20 | 24 | 40 | 48;
     gl: MaterialSymbol;
 };
 
-const icon = cva('line-he inline-block text-nowrap not-italic leading-none', {
+const icon = cva('inline-block text-nowrap not-italic', {
     variants: {
         size: {
-            sm: 'text-[20px]',
-            md: 'text-[24px]',
-            lg: 'text-[40px]',
-            xl: 'text-[48px]',
+            sm: 'text-[20px] leading-none',
+            md: 'text-[24px] leading-none',
+            lg: 'text-[40px] leading-none',
+            xl: 'text-[48px] leading-none',
         },
         type: {
             'material-symbols-outlined': 'font-material-symbols-outlined',
@@ -38,10 +42,22 @@ const Icon = ({ type, size, ...prop }: IconProps) => {
         type === 'material-symbols-rounded' ||
         type === 'material-symbols-sharp'
     ) {
-        const { fill = false, weight = 400, grade = 0, opticalSize = 24, gl } = prop;
+        const {
+            fill = false,
+            weight = 400,
+            grade = 0,
+            opticalSize = 24,
+            gl,
+        } = {
+            ...prop,
+            weight: prop.weight || 400,
+            grade: prop.grade || 0,
+            opticalSize: prop.opticalSize || 24,
+        };
         return (
             <i
-                className={icon({ type, size })}
+                translate={'no'}
+                className={twMerge(icon({ type, size }), prop.className)}
                 style={{
                     fontVariationSettings: `'FILL' ${
                         fill ? 1 : 0
